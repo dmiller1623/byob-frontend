d07ead0f894ff1d5521135abb26d2682ee20ba9e
Dennis <dmiller1623@gmail.com>
2018-10-15 14:39:47 -0600

    Add pokemon data

---
+++ src/utilities/pokemonData.js
@@ -0,0 +1,94 @@
+const pokemon = [
+  {
+    name: 'Charizard',
+    type: 'fire',
+    img: '../../images/charizard.png',
+    hp: 78,
+    speed: 100,
+    attack: 84,
+    defense: 78
+  },
+  {
+    name: 'Blastoise',
+    type: 'water',
+    img: '../../images/blastoise.png',
+    hp: 79,
+    speed: 78,
+    attack: 83,
+    defense: 100
+  },
+  {
+    name: 'Pikachu',
+    type: 'Electric',
+    img: '../../images/pikachu.png',
+    hp: 35,
+    speed: 90,
+    attack: 55,
+    defense: 40
+  },
+  {
+    name: 'Kadabra',
+    type: 'Psychic',
+    img: '../../images/kadabra.png',
+    hp: 40,
+    speed: 105,
+    attack: 35,
+    defense: 30
+  },
+  {
+    name: 'Pidgeot',
+    type: 'flying',
+    img: '../../images/pidgeot.png',
+    hp: 83,
+    speed: 101,
+    attack: 80,
+    defense: 75
+  },
+  {
+    name: 'Gengar',
+    type: 'ghost',
+    img: '../../images/gengar.png',
+    hp: 60,
+    speed: 110,
+    attack: 65,
+    defense: 60
+  },
+  {
+    name: 'Gyarados',
+    type: 'water',
+    img: '../../images/gyardos.png',
+    hp: 95,
+    speed: 81,
+    attack: 125,
+    defense: 79
+  },
+  {
+    name: 'Dragonite',
+    type: 'dragon',
+    img: '../../images/dragonite.png',
+    hp: 91,
+    speed: 80,
+    attack: 134,
+    defense: 95
+  },
+  {
+    name: 'Mewtwo',
+    type: 'psychic',
+    img: '../../images/mewtwo.png',
+    hp: 106,
+    speed: 130,
+    attack: 110,
+    defense: 90
+  },
+  {
+    name: 'Zapados',
+    type: 'electric',
+    img: '../../images/zapados.png',
+    hp: 90,
+    speed: 100,
+    attack: 90,
+    defense: 85
+  }
+]
+
+export default pokemon
