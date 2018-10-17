6b3ac0f2185273e9bc23652d7334a3e8f0fcfc41
Dennis <dmiller1623@gmail.com>
2018-10-17 14:32:46 -0600

    Add refix the load issue

--- src/components/trainersSideBar/TrainersSideBar.js
+++ src/components/trainersSideBar/TrainersSideBar.js
@@ -14,28 +14,28 @@ class TrainersSideBar extends Component {
         <img className='trainer-header' alt='trainer title' src={Trainers} />
         <button onClick={toggleAddTrainer}>Add Trainer</button>
         <div>
-          {trainers.length && trainersTeams.length && trainers.map((trainer, index) => {
-            const foundTeams = trainersTeams.filter((team) => {
-              return trainer.id === team.trainer_id;
-            });
-            return <div key={index}>
-              <div className='name-header'>
-                <h2>Name: <span>{trainer.name}</span></h2>
-                <h2>Level: <span>{trainer.level}</span></h2>
-                <img className='delete-buttons' alt='delete button' src={Delete} onClick={() => deleteTrainer(trainer.id)}/>
-              </div>
-              {foundTeams.map((pokemon, index) => {
-                return <div key={index} className='team-display'>
-                  <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_one}/>
-                  <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_two}/>
-                  <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_three}/>
-                  <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_four}/>
-                  <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_five}/>
-                  <img className='delete-buttons' src={Delete} alt='delete button' onClick={() => deletePokemonTeam(pokemon.id)}/>
+          {trainers.map((trainer, index) => {
+                const foundTeams = trainersTeams.filter((team) => {
+                  return trainer.id === team.trainer_id;
+                });
+                return <div key={index}>
+                  <div className='name-header'>
+                    <h2>Name: <span>{trainer.name}</span></h2>
+                    <h2>Level: <span>{trainer.level}</span></h2>
+                    <img className='delete-buttons' alt='delete button' src={Delete} onClick={() => deleteTrainer(trainer.id)}/>
+                  </div>
+                  {foundTeams.map((pokemon, index) => {
+                    return <div key={index} className='team-display'>
+                      <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_one}/>
+                      <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_two}/>
+                      <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_three}/>
+                      <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_four}/>
+                      <img className='sidebar-images' alt='pokemon' src={pokemon.pokemon_five}/>
+                      <img className='delete-buttons' src={Delete} alt='delete button' onClick={() => deletePokemonTeam(pokemon.id)}/>
+                    </div>;
+                  })}
                 </div>;
               })}
-            </div>;
-          })}
         </div>
       </div>
     );
